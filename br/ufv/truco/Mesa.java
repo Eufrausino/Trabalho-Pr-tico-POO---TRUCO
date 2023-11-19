db56bef0889606d73a73aa2339c413e540a94946
CarlosMMCosta <carlos.costa1@ufv.br>
2023-11-19 18:41:26 -0300

    Adicionadas as classes Mesa e Truco
    Ainda falta relacionar com as outras classes

---
+++ br/ufv/truco/Mesa.java
@@ -0,0 +1,21 @@
+package br.ufv.truco;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Mesa {
+	private Stack<Carta> cartas;
+	private ArrayList<Carta> descarte;
+	
+	public Mesa() {
+		cartas = new Stack<Carta>();
+		descarte = new ArrayList<Carta>();
+	}
+	
+	public void receber(Carta c) {
+		
+	}
+	
+	
+}
---
+++ br/ufv/truco/Truco.java
@@ -0,0 +1,27 @@
+package br.ufv.truco;
+
+public class Truco {
+	public static void main(String[] args) {
+		
+		// instanciando a tela do jogo
+		//TODO TelaDeJogo		
+		
+		// instanciando os jogadores
+		Jogador jogador1 = new Jogador("J1", false);
+		Jogador jogador2 = new Jogador("J2", true);
+		Jogador jogador3 = new Jogador("J3", true);
+		Jogador jogador4 = new Jogador("J4", true);
+		
+		// instanciando as equipes
+		Equipe equipe1 = new Equipe(1, jogador1, jogador2);
+		Equipe equipe2 = new Equipe(2, jogador3, jogador4);
+		
+		// instanciando o baralho
+		Baralho baralho = new Baralho();
+		
+		// instanciando a partida
+		Partida partida = new Partida(equipe1, equipe2, baralho);
+		
+		partida.executaPartida();
+	}
+}
