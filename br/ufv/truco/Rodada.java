319def583a8670068052201eb113fb178a150357
Gustavo Eufrausino <108813352+Eufrausino@users.noreply.github.com>
2023-11-28 20:37:49 -0300

    Atualização da classe Rodada
    Tentei refatorar a classe, mas não deu para fugir muito dos switch. Para terminar a classe preciso conseguir associar a carta vencedora ao jogador e então a equipe.

--- br/ufv/truco/Rodada.java
+++ br/ufv/truco/Rodada.java
@@ -39,17 +39,14 @@ public class Rodada
 		return trucado;
 	}
 	
-	public void trucar(Jogador alvo)
+	public void trucar(Jogador alvo)//Certo
 	{
 		System.out.println("Truco!");
 		trucado++;
 	}
-	
+
 	public char resposta(Jogador alvo)
 	{
-		//Como o retorno original era um char, mantive assim
-		//S = seis, N = nove e D = doze
-		
 		this.leitor = new Scanner(System.in);
 		System.out.println("Corre, aceita ou pede mais?");
 		System.out.println("Entre com 0 para correr, 1 para aceitar ou 2 para trucar: ");
@@ -57,6 +54,9 @@ public class Rodada
 		
 		switch(resposta)
 		{
+			case 0:
+				System.out.println("Correu!");
+				return 'C';
 			case 1:
 				System.out.println("Aceitou!");
 				return 'A';
@@ -81,23 +81,176 @@ public class Rodada
 			default:
 				break;
 		}
-		
 		leitor.close();
+		return 'E';//erro
+	}
+
+	public Carta jogar(Jogador jogador, int turno)
+	{
+		boolean coberta = false;
+
+		if(turno >= 1)
+		{
+			System.out.println("Jogar encoberta? Entre com 0 para não OU 1 para sim");
+			int acobertada = leitor.nextInt();
+
+			switch (acobertada) 
+			{
+				case 0:
+					coberta = false;
+					break;
+			
+				case 1:
+					coberta = true;
+					break;
+				default:
+					break;
+			}
+
+			return jogador.jogarCarta(coberta);
+		}
+
+		return jogador.jogarCarta(coberta);
+	}
+
+	public int ConfrontoTruco(Jogador j1, Jogador j2)
+	{
+		this.resposta(j2);
+
+		switch(this.trucado)
+		{
+			case 1:
+				this.resposta(j1);
+				switch (this.resposta(j1)) 
+				{
+					case 'C':
+						return 0;
+
+					case 'S':
+						this.resposta(j2);
+						switch (this.resposta(j2))
+						{
+							case 'C':
+								return 0;
+							
+							case 'N':
+								this.resposta(j1);
+								switch (this.resposta(j1)) 
+								{
+									case 'C':
+										return 0;
+
+									case 'D':
+										switch (this.resposta(j2))
+										{
+											case 'C':
+												return 0;
+											default:
+												return 1;
+										}
+								
+									default:
+										return 1;
+								}
+							default:
+								return 1;
+						}
+						default:
+							return 1;
+					}
+			case 2:
+			this.resposta(j1);
+			switch (this.resposta(j1))
+			{
+				case 'C':
+					return 0;
+				
+				case 'N':
+					this.resposta(j2);
+					switch (this.resposta(j1)) 
+					{
+						case 'C':
+							return 0;
+
+						case 'D':
+							switch (this.resposta(j2))
+							{
+								case 'C':
+									return 0;
+								default:
+									return 1;
+							}
+					
+						default:
+							return 1;
+					}
+				
+					default:
+						return 1;
+				}
+
+				case 3:
+				this.resposta(j1);
+				switch (this.resposta(j1)) {
+					case 'C':
+						return 0;
+					case 'D':
+						this.resposta(j2);
+						switch (this.resposta(j2)) 
+						{
+							case 'C':
+								return 0;
+						
+							default:
+								return 1;
+						}
+					default:
+						return 1;
+				}
+			
+				default:
+					return 1;
+		}
+	}
+
+	public void declaraVencedor(Carta c1, Carta c2, Carta c3, Carta c4)
+	{
+		ArrayList<Carta> cartasNaMesa = new ArrayList<Carta>();
+
+		cartasNaMesa.add(c1);
+		cartasNaMesa.add(c2);
+		cartasNaMesa.add(c3);
+		cartasNaMesa.add(c4);
+
+		Carta maior = cartasNaMesa.get(0);
 		
-		System.out.println("Correu!");
-		return 'C';
+		for(int i = 1; i <= cartasNaMesa.size(); i++)
+		{
+			if(!maior.ganhaDe(cartasNaMesa.get(i)))
+			{
+				maior = cartasNaMesa.get(i);
+			}
+		}
+
+		//TODO: Falta associar a carta ao jogador que a jogou
+
 	}
 	
 	public void executaRodada(Equipe equipe1, Equipe equipe2)
 	{
 		this.leitor = new Scanner(System.in);
-		int cont = 0;
 		
 		ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
-		jogadores.add(equipe1.getJogador1);
-		jogadores.add(equipe1.getJogador2);
-		jogadores.add(equipe2.getJogador1);
-		jogadores.add(equipe2.getJogador2);
+		jogadores.add(equipe1.getJogador1());
+		jogadores.add(equipe1.getJogador2());
+		jogadores.add(equipe2.getJogador1());
+		jogadores.add(equipe2.getJogador2());
+
+		int turno = 0;
+		int atual = 1;
+		int anterior = atual - 1;
+
+		boolean correu = false;
 		
 		ArrayList<Carta> cartasJogadas = new ArrayList<Carta>();
 		
@@ -106,160 +259,51 @@ public class Rodada
 			System.out.println("Jogar ou pedir truco?");
 			System.out.println("Entre com 0 para jogar ou 1 para pedir truco: ");
 			int resposta = leitor.nextInt();
-			boolean coberta = false;
-			cont++;
-			
+
 			switch(resposta)
 			{
 				case 0:
-					System.out.println("Jogar carta encoberta ou não?");
-					System.out.println("0 para não/1 para sim: ");
-					int jogarEncoberta = leitor.nextInt();
-					switch(jogarEncoberta)
-					{
-						case 0:
-							coberta = false;
-							j.jogarCarta(coberta);
-							cartasJogadas.add(j.jogarCarta(coberta));
-							break;
-						case 1:
-							coberta = true;
-							j.jogarCarta(coberta);
-							cartasJogadas.add(j.jogarCarta(coberta));
-							break;
-						default:
-							break;
-					}
+					this.jogar(j, turno);
+					cartasJogadas.add(this.jogar(j, turno));
+					break;
 				case 1:
-					//j.pedirTruco();
-					this.trucar(jogadores.get(cont));
-					while(true)
-					{
-						this.resposta(j);
-						if(this.resposta(j) == 'C')
-						{
-							//perde pts;
-							return;
-						}
-						
-						else if(this.resposta(j) == 'A')
-						{
-							//jogo continua normalmente
-							break;
-						}
-						
-						else if(this.resposta(j) == 'S')
-						{
-							this.resposta(jogadores.get(cont));
-							switch(this.resposta(jogadores.get(cont)))
-							{
-								case 'C':
-									//perde pts
-									return;
-								case 'N':
-									this.resposta(j);
-									switch(this.resposta(jogadores.get(cont)))
-									{
-										case 'C':
-											//perde pts
-											return;
-										case 'D':
-											this.resposta(j);
-											switch(this.resposta(j))
-											{
-												case 'C':
-													//perde pts
-													return;
-												default:
-													break;
-											}
-											break;
-										default:
-											break;
-									}
-									break;
-								default:
-									break;
-							}
-							break;
-						}
-						
-						else if(this.resposta(j) == 'N')
-						{
-							this.resposta(jogadores.get(cont));
-							switch(this.resposta(jogadores.get(cont)))
-							{
-								case 'C':
-									//perde pts
-									return;
-								case 'D':
-									this.resposta(j);
-									switch(this.resposta(j))
-									{
-										case 'C':
-											return;
-										default:
-											break;
-									}
-									break;
-								default:
-									break;
-							}
-							break;
-						}
-						
-						else if(this.resposta(j) == 'D')
-						{
-							this.resposta(jogadores.get(cont));
-							switch(this.resposta(jogadores.get(cont)))
-							{
-								case 'C':
-									//perde pts
-									return;
-								default:
-									break;
-							}
-							break;
-						}
-					}
-					System.out.println("Jogar carta encoberta ou não?");
-					System.out.println("0 para não/1 para sim: ");
-					int JogarEncoberta = leitor.nextInt();
-					switch(JogarEncoberta)
+					this.ConfrontoTruco(jogadores.get(atual), jogadores.get(anterior));
+					switch(this.ConfrontoTruco(jogadores.get(atual), jogadores.get(anterior)))
 					{
 						case 0:
-							coberta = false;
-							j.jogarCarta(coberta);
-							cartasJogadas.add(j.jogarCarta(coberta));
+							System.out.println("Correu!");
+							correu = true;
 							break;
 						case 1:
-							coberta = true;
-							j.jogarCarta(coberta);
-							cartasJogadas.add(j.jogarCarta(coberta));
+							System.out.println("Aceitou!");
+							this.jogar(j, turno);
+							cartasJogadas.add(this.jogar(j, turno));
 							break;
 						default:
 							break;
-					}	
+					}
+				default:
+					break;
 			}
-		}
-		
-		Carta maior = cartasJogadas.get(0);
-		for(int i = 1; i <= cartasJogadas.size(); i++)
-		{
-			if(maior.compara(maior) < cartasJogadas.get(i).compara(cartasJogadas.get(i)))
+
+			if(correu == true)
 			{
-				maior = cartasJogadas.get(i);
+				break;
 			}
+
+			turno++;
+			anterior = atual;
+			atual++;
 		}
-		//Associar jogador com a carta;
-		//Depois que descobrimos qual jogador que jogou a carta, saberemos a qual equipe ele pertence
+
+		//TODO: encontrar o vencedor da rodada. Carta -> Jogador -> Equipe
 	}
-	
-	public Jogador definirVencedor()
+
+	/*public Jogador definirVencedor()
 	{
 		//Comparar a maior carta da mesa -> associar ela ao jogador que a jogou -> associar o
 		//jogador a equipe;
 		//Iniciar nova rodada a partir desse jogador;
 		
-	}
+	}*/
 }
