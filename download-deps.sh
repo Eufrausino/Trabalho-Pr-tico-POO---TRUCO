60e07420ddfcf832183d9022744650c90071c30e
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2023-12-10 17:25:00 -0300

    Corrige o script de execução de testes
    Agora ele funciona direitinho

--- download-deps.sh
+++ download-deps.sh
@@ -2,6 +2,6 @@
 # download-deps.sh: para os hipsters que insistem em trabalhar com
 # desenvolvimento java sem por as mãos em nenhuma IDE. Esse script realiza o
 # download do JUnit e suas dependências, necessárias à execução dos testes.
-wget https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar > deps/junit-4.13.2.jar
-wget http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar > deps/hamcrest-core-1.3.jar
-rm *.jar # remove artefatos temporários do wget
+mkdir -p deps/
+wget https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar -O deps/junit-4.13.2.jar
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/hamcrest/hamcrest-all-1.3.jar -O deps/hamcrest-1.3.jar
